4429c77e84d21e52445fd177b6e45d92bda3e088
PiovaD <dario.piovani95@gmail.com>
2022-07-05 18:05:34 +0200

    tetris

--- m1/w3/d2/esercitazione-w3d2/js/script.js
+++ m1/w3/d2/esercitazione-w3d2/js/script.js
@@ -57,33 +57,110 @@ function isPrime() {
 
 //game
 
-let mat = [['-','-','-'],['-','-','-'],['-','-','-']];
- let cont = 1;
+let mat = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
+let cont = 1;
 console.log(mat);
 
-function stamp(){
-  document.querySelector('.top').innerHTML = (mat[0][0]+ '-' + mat[0][1] + '-' + mat[0][2]);
-  document.querySelector('.mid').innerHTML = (mat[1][0]+ '-' + mat[1][1] + '-' + mat[1][2]);
-  document.querySelector('.bot').innerHTML = (mat[2][0]+ '-' + mat[2][1] + '-' + mat[2][2]);
+function stamp() {
+    document.querySelector('.top').innerHTML = (mat[0][0] + '-' + mat[0][1] + '-' + mat[0][2]);
+    document.querySelector('.mid').innerHTML = (mat[1][0] + '-' + mat[1][1] + '-' + mat[1][2]);
+    document.querySelector('.bot').innerHTML = (mat[2][0] + '-' + mat[2][1] + '-' + mat[2][2]);
 }
 
+function reset() {
+    mat = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
+    stamp();
+    cont = 1;
+    document.querySelector('.messaggio').innerHTML = "tocca  a x";
+}
 stamp()
 
-function verifica(){
-    let x = document.querySelector('#asse-x').value;
-    let y = document.querySelector('#asse-y').value;
-    console.log(x,y);
-    if((x>0 && x<4)&&(y>0 && y<4)){
+function verifica() {
+    if (!(win())) {
+        let x = document.querySelector('#asse-x').value;
+        let y = document.querySelector('#asse-y').value;
+        console.log(x, y);
+        x--;
+        y--;
+        if ((x >= 0 && x <= 3) && (y => 0 && y <= 3)) {
 
-        
+            if (mat[x][y] == '-') {
+                if (cont % 2) {
+                    mat[x][y] = 'x';
+                    stamp();
+                } else {
+                    mat[x][y] = 'o';
 
-        cont++
-        if (!(cont%2)){
-            return document.querySelector('.messaggio').innerHTML = "tocca  a o";
-        }else{
-            return document.querySelector('.messaggio').innerHTML = "tocca  a x";
+                    stamp();
+                }
+
+                cont++
+
+            } else {
+                return document.querySelector('.messaggio').innerHTML = "cella occupata riprova";
+            }
+
+            if (!(win())) {
+                if (!(cont % 2)) {
+                    return document.querySelector('.messaggio').innerHTML = "tocca  a o";
+                } else {
+                    return document.querySelector('.messaggio').innerHTML = "tocca  a x";
+                }
+            } else {
+                return document.querySelector('.messaggio').innerHTML = (mat[x][y] + " hai vinto");
+            }
+
+
+        } else {
+            return document.querySelector('.messaggio').innerHTML = "inserimento errato riprova";
         }
-    }else{
-        return document.querySelector('.messaggio').innerHTML = "inserimento errato riprova";
+    } else {
+        return document.querySelector('.messaggio').innerHTML = "Resetta la partita";
     }
+}
+
+
+function win() {
+
+    if ((mat[0][1] == mat[0][0]) && (mat[0][1] == mat[0][2]) && (mat[0][1] != '-')) { //prima riga
+        console.log('win');
+        return true;
+    }
+    
+    if ((mat[1][1] == mat[1][0]) && (mat[1][1] == mat[1][2]) && (mat[1][1] != '-')) { //seconda riga
+        console.log('win');
+        return true;
+    }
+
+    if ((mat[2][1] == mat[2][0]) && (mat[2][1] == mat[2][2]) && (mat[2][1] != '-')) { //terza riga
+        console.log('win');
+        return true;
+    }
+    
+    if ((mat[1][0] == mat[0][0]) && (mat[1][0] == mat[2][0]) && (mat[1][0] != '-')) { //prima colonna
+        console.log('win');
+        return true;
+    }
+
+    if ((mat[1][1] == mat[0][1]) && (mat[1][1] == mat[2][1]) && (mat[1][1] != '-')) { //seconda colonna
+        console.log('win');
+        return true;
+    }
+
+    if ((mat[1][2] == mat[0][2]) && (mat[1][2] == mat[2][2]) && (mat[2][1] != '-')) { //terza colonna
+        console.log('win');
+        return true;
+    }
+
+    if ((mat[1][1] == mat[0][0]) && (mat[1][1] == mat[2][2]) && (mat[1][1] != '-')) { //diagonale \
+        console.log('win');
+        return true;
+    }
+
+    if ((mat[1][1] == mat[0][2]) && (mat[1][1] == mat[2][0]) && (mat[1][1] != '-')) { //diagonale /
+        console.log('win');
+        return true;
+    }
+
+    return false;
 }
