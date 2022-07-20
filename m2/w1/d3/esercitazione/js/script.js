b90d1ade0f2f4868d465b2fda783d90fa617ca4e
PiovaD <dario.piovani95@gmail.com>
2022-07-20 18:31:00 +0200

    fix

--- m2/w1/d3/esercitazione/js/script.js
+++ m2/w1/d3/esercitazione/js/script.js
@@ -1,32 +1,49 @@
 //funzione rotazione 
 document.querySelectorAll('.btn').forEach(button => {
     button.addEventListener('click', function () {
-
-        let rotazione = button.getAttribute('data-rotate')
-
-        let container = document.querySelector('#container');
-
-        let angle = parseInt(container.getAttribute('data-angle'));
-
-        console.log(angle);
-
-        switch (rotazione) {
-
-            case 'l':
-                angle -= 90;
-                break;
-
-            case 'r':
-                angle += 90;
-                break;
-        }
-
-        rotate(angle);
-        container.setAttribute('data-angle', angle);
-
-    });
-
+        movment(90, button)
+    })
 });
+
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('mouseover',  function () {
+        movment(45, button)
+    })
+})
+
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('mouseout',  function () {
+        movment(-45, button)
+    })
+})
+
+
+function movment(set, button) {
+    console.log(set)
+
+    let rotazione = button.getAttribute('data-rotate')
+
+    let container = document.querySelector('#container');
+
+    let angle = parseInt(container.getAttribute('data-angle'));
+
+    console.log(angle);
+
+    switch (rotazione) {
+
+        case 'l':
+            angle -= set;
+            break;
+
+        case 'r':
+            angle += set;
+            break;
+    }
+
+    rotate(angle);
+    container.setAttribute('data-angle', angle);
+
+}
 
 function rotate(angle) {
 
@@ -35,11 +52,3 @@ function rotate(angle) {
     container.style.transform = `rotateY(${angle}deg)`;
 
 }
-
-//funzione hov
-document.querySelectorAll('.btn').forEach(button => {
-    button.addEventListener('mouseover', function () {
-
-    });
-
-});
