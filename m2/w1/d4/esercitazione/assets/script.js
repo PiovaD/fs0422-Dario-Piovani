2c66e0925b9c894dbc4bbdec1e878d97c2952d62
PiovaD <dario.piovani95@gmail.com>
2022-07-21 22:49:22 +0200

    fix

--- m2/w1/d4/esercitazione/assets/script.js
+++ m2/w1/d4/esercitazione/assets/script.js
@@ -1,50 +1,137 @@
-let qs = (element) => document.querySelector(element)
+// abbreviazione per query selector
+let qs = (element) => document.querySelector(element);
 
-let qsA = (element) => document.querySelectorAll(element)
+// abbreviazione per query selector all
+let qsA = (element) => document.querySelectorAll(element);
 
-qs('.dropdown').addEventListener('click', () => qs('#myDropdown').classList.toggle('active'))
+//toggle del menu di formattzione al inizio della pagina
+qs('.dropbtn').addEventListener('click', () => qs('.myDropdown').classList.toggle('active'));
 
+// menu di formattazione a inizio pagina
+qsA('.formattazione').forEach(element => element.addEventListener('click', () => modifica(element, '#esercizio')));
 
-qsA('.formattazione').forEach(element => {
-    element.addEventListener('click', () => {
+//chiama la funzione per aggiungere elementi
+qs('#create-list').addEventListener('click', add);
 
-        let modifier = element.getAttribute('data-style');
-        let sect = qs('#esercizio')
+//prende l'invio come comando per aggiungere elementi
+qs('#insert').addEventListener('keypress', (event) => event.keyCode === 13 && add());
 
+//distrugge lintera lista
+qs('#destroy-list').addEventListener('click', () => qs('#lista').innerHTML = '')
+
+//rimuove elementi dalla lista
+let remove = (li) => li.remove();
+
+//attiva e disattiva i dropdown
+let toggle = (div) => div.classList.toggle('active');
+
+//modificatrore di stile
+function modifica(element, container) {
+
+    let modifier = element.getAttribute('data-style');
+
+    typeof container != 'string' ? sect = container : sect = qs(container)
+
+    switch (modifier) {
+        case 'italic':
+            sect.style.fontStyle != 'italic' ?
+                sect.style.fontStyle = 'italic' :
+                sect.style.fontStyle = ''
+            break
+
+        case 'bold':
+            sect.style.fontWeight != 'bold' ?
+                sect.style.fontWeight = 'bold' :
+                sect.style.fontWeight = ''
+            break
+
+        case 'underline':
+            decoration(modifier, sect)
+            break
+
+        case 'line-through':
+            decoration(modifier, sect)
+            break
+
+        case 'reset':
+            sect.style.fontStyle = ''
+            sect.style.fontWeight = ''
+            sect.style.textDecoration = ''
+            break
+
+        case 'delete':
+            remove(container);
+            break
+    }
+
+}
+
+//gestisco text decoration uguali
+function decoration(decoration, sect) {
+    console.log(sect.style.textDecoration.search(decoration))
+    if (sect.style.textDecoration == decoration) {
         sect.style.textDecoration = ''
-        sect.style.fontStyle = ''
-        sect.style.fontWeight = ''
 
-        switch (modifier) {
-            case 'italic':
-                sect.style.fontStyle = 'italic'
-                break
-            case 'bold':
-                sect.style.fontWeight = 'bold'
-                break
-            case 'underline':
-                sect.style.textDecoration = 'underline'
-                break
-        }
+    } else if (sect.style.textDecoration.search(decoration) > -1) {
+        sect.style.textDecoration = sect.style.textDecoration.replace(decoration, '');
 
-    })
-})
+    } else {
+        sect.style.textDecoration += ' ' + decoration
 
-qs('#create-list').addEventListener('click', () => {
+    }
+}
 
-    let toDoList = qs('#to-do')
-    let insert = qs('#insert')
+//aggiunge elemento a lista
+function add() {
+
+    let toDoList = qs('#lista');
+    let insert = qs('#insert');
     let impegno = insert.value;
 
-    let li = document.createElement('li')
-
     if (impegno) {
-        li.addEventListener('click', () => li.remove())
-        li.innerHTML = impegno
-        toDoList.append(li)
+
+        let li = document.createElement('li');
+
+        let div = document.createElement('div');
+        let transform = ['underline', 'bold', 'line-through', 'reset', 'delete'];
+        div.classList.add('myDropdown');
+
+        for (let style of transform) {
+
+            let p = document.createElement('p')
+
+            p.setAttribute('data-style', style)
+
+            switch (style) {
+                case 'underline':
+                    p.innerHTML = 'Sottolinea'
+                    break
+                case 'bold':
+                    p.innerHTML = 'Grassetto'
+                    break
+                case 'line-through':
+                    p.innerHTML = 'Barra'
+                    break
+                case 'reset':
+                    p.innerHTML = 'Reset dello stile'
+                    break
+                case 'delete':
+                    p.innerHTML = 'Cancella'
+                    break
+            }
+
+            console.log(li);
+            p.addEventListener('click', () => modifica(p, li));
+
+            div.append(p)
+        }
+
+        li.addEventListener('click', () => toggle(div));
+        li.innerHTML = '🔶 ' + String(impegno);
+        li.append(div);
+        toDoList.append(li);
     }
 
     insert.value = '';
-
-})
+}
 
