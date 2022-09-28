34419dc991bc1db66fee0c48a42c770b3d8feb21
PiovaD <dario.piovani95@gmail.com>
2022-09-28 15:37:07 +0200

    fix

--- m4/w1/d3/esercitazione/src/es3/Esercizio3.java
+++ m4/w1/d3/esercitazione/src/es3/Esercizio3.java
@@ -6,20 +6,24 @@ public class Esercizio3 {
 
 	public static void main(String[] args) {
 
-		String str  = "";
+		String str = "";
 		Scanner in = new Scanner(System.in);
-		
+
 		System.out.println("Inserisci stringa: ");
 		str = in.nextLine();
 
-		while (!str.equals(":q")){
-						
-			String newStr = str.replaceAll("", ",");
-			
-			System.out.println(newStr);
-			
+		while (!str.equals(":q")) {
+
+			// String newStr = str.replaceAll("", ",");
+
+			String[] newStr = str.split("");
+
+			for (String ch : newStr) {
+				System.out.printf(ch + ",");
+			}
+			System.out.println();
 			System.out.println("Inserisci stringa: ");
-			str = in.nextLine();			
+			str = in.nextLine();
 		}
 
 		System.out.println("the end");
